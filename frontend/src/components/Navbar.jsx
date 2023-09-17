8a10af22d96aee31bc643c4a73f30b258ffa0c31
Kamil Holub <kahol20000@gmail.com>
2023-09-17 10:47:35 -0400

    forgot to save navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,14 +4,18 @@ import { Link } from 'react-router-dom';
 
 function AppNavbar() {
   return (
+
     <Navbar bg="dark" variant="dark">
+      <link rel="stylesheet" href="../../public/style.css" />
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"></link>
+
       <Navbar.Brand as={Link} to="/">Navbar</Navbar.Brand>
       <Nav className="mr-auto">
         <Nav.Link as={Link} to="/">Home</Nav.Link>
         <Nav.Link as={Link} to="/about">About</Nav.Link>
       </Nav>
-      <Form inline>
-        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Form>
+        <FormControl type="text" placeholder="Search" />
         <Button variant="outline-light">Search</Button>
       </Form>
     </Navbar>
