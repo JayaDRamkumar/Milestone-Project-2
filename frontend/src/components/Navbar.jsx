82aad9d4eb30664c51f3f15389e6d8d15ac67c35
Kamil Holub <kahol20000@gmail.com>
2023-09-17 10:48:54 -0400

    forgot to save navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 function AppNavbar() {
   return (
 
-    <Navbar bg="dark" variant="darkgit ">
+    <Navbar bg="dark" variant="dark">
       <link rel="stylesheet" href="../../public/style.css" />
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"></link>
 
